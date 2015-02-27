a80d9ae9e7adc008f786d62e8c6017f4864262e6
Daniel A. <dadenew.yyt@gmail.com>
2015-02-27 15:06:52 +0300

    Update authorize_net.php

--- config/authorize_net.php
+++ config/authorize_net.php
@@ -3,6 +3,7 @@
 // Authorize.net Account Info
 $config['api_login_id'] = '';
 $config['api_transaction_key'] = '';
+$config['api_email'] = ''; //this the email you are going to use either ofor the sandbox or production setting
 $config['api_url'] = 'https://test.authorize.net/gateway/transact.dll'; // TEST URL
 //$config['api_url'] = 'https://secure.authorize.net/gateway/transact.dll'; // PRODUCTION URL
 
